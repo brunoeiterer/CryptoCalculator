dd6829306888d1205b335a848dd0796816e16ef6
unknown <brunoeiterer@gmail.com>
2020-08-20 20:04:15 -0300

    Setup gradle build

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("CryptoCalculator");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
