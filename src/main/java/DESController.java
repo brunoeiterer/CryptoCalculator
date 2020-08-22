336efe05c57ddc213258c90206029a44693332cd
unknown <brunoeiterer@gmail.com>
2020-08-22 20:00:18 -0300

    Add DES ECB decryption

--- src/main/java/DESController.java
+++ src/main/java/DESController.java
@@ -89,7 +89,7 @@ public class DESController{
         }
         else{
             try{
-                Cipher cipher = Cipher.getInstance("DES");
+                Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
                 try{
                     SecretKeySpec key = new SecretKeySpec(Hex.decodeHex(this.DESKeyTextField.getText().trim().toCharArray()), "DES");
                     cipher.init(Cipher.ENCRYPT_MODE, key);
@@ -119,6 +119,63 @@ public class DESController{
     }
 
     private void DecryptButtonHandler(ActionEvent event){
+        StringBuilder errorMessage = new StringBuilder();
+        if((this.DESInputTextArea.getText().trim()).isEmpty()){
+            errorMessage.append("The input is invalid.\n");
+        }
+        else if((this.DESInputTextArea.getText().trim()).length() % 16 != 0){
+            errorMessage.append("The input size must be a multiple of 8 bytes.\n");
+        }
+        else if(!(this.DESInputTextArea.getText().trim()).matches("[A-Fa-f0-9]+")){
+            errorMessage.append("The input must be an hex string [A-F a-f 0-9].\n");
+        }
 
+        if((this.DESKeyTextField.getText().trim()).isEmpty()){
+            errorMessage.append("\nThe key is invalid.\n");
+        }
+        else if((this.DESKeyTextField.getText().trim()).length() != 16){
+            errorMessage.append("The key size must be 8 bytes\n");
+        }
+        else if(!(this.DESKeyTextField.getText().trim()).matches("[A-Fa-f0-9]+")){
+            errorMessage.append("The key must be an hex string [A-F a-f 0-9].\n");
+        }
+
+        if(!errorMessage.toString().isEmpty()){
+            Alert invalidInputAlert = new Alert(Alert.AlertType.ERROR);
+            invalidInputAlert.setHeaderText(null);
+            invalidInputAlert.setGraphic(null);
+            invalidInputAlert.setTitle("Invalid Input");
+            invalidInputAlert.setContentText(errorMessage.toString());
+            invalidInputAlert.showAndWait();
+        }
+        else{
+            try{
+                Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
+                try{
+                    SecretKeySpec key = new SecretKeySpec(Hex.decodeHex(this.DESKeyTextField.getText().trim().toCharArray()), "DES");
+                    cipher.init(Cipher.DECRYPT_MODE, key);
+                    byte ciphertext[] = cipher.doFinal(Hex.decodeHex(this.DESInputTextArea.getText().trim().toCharArray()));
+                    this.DesOutputTextArea.setText(Hex.encodeHexString(ciphertext));
+                }
+                catch(InvalidKeyException e){
+                    e.printStackTrace();
+                }
+                catch(IllegalBlockSizeException e){
+                    e.printStackTrace();
+                }
+                catch(BadPaddingException e){
+                    e.printStackTrace();
+                }
+                catch(DecoderException e){
+                    e.printStackTrace();
+                }
+            }
+            catch(NoSuchAlgorithmException e){
+                e.printStackTrace();
+            }
+            catch(NoSuchPaddingException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
