14f56aeab54cf6944780b04656c85ad3bdadada1
unknown <brunoeiterer@gmail.com>
2020-09-05 13:19:23 -0300

    Add DESCFB8PKCS5Encrypt test

--- src/test/java/DESTest.java
+++ src/test/java/DESTest.java
@@ -266,4 +266,30 @@ class DESTests {
         /* verify if encryption was successful */
         FxAssert.verifyThat("#DesOutputTextArea", TextInputControlMatchers.hasText("5679535718144520"));
     }
+
+    @Test
+    void DESCFB8PKCS5Encrypt(FxRobot robot) {
+        /* select ECB Mode of Operation */
+        robot.clickOn("#DesModeOfOperationComboBox");
+        robot.clickOn("CFB8");
+
+        /* select noPadding */
+        robot.clickOn("#DesPaddingComboBox");
+        robot.clickOn("PKCS5Padding");
+
+        /* write input */
+        robot.clickOn("#DESInputTextArea").write("0000000000000000");
+
+        /* write ICV */
+        robot.clickOn("#DESICVTextField").write("0123456789ABCDEF");
+
+        /* write key */
+        robot.clickOn("#DESKeyTextField").write("0123456789ABCDEF");
+
+        /* click on Encrypt button */
+        robot.clickOn("#DESEncryptButton");
+
+        /* verify if encryption was successful */
+        FxAssert.verifyThat("#DesOutputTextArea", TextInputControlMatchers.hasText("5679535718144520e5c0add3ad86f4f4"));
+    }
 }
