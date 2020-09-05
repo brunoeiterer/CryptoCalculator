7d4d4aa359ba21a20844161e037120c5a7db8a50
unknown <brunoeiterer@gmail.com>
2020-09-05 13:06:38 -0300

    Add DESCBCEncryptNoPadding test

--- src/test/java/DESTest.java
+++ src/test/java/DESTest.java
@@ -136,4 +136,30 @@ class DESTests {
         /* verify if decryption was successful */
         FxAssert.verifyThat("#DesOutputTextArea", TextInputControlMatchers.hasText("0000000000000000"));
     }
+
+    @Test
+    void DESCBCEncryptNoPadding(FxRobot robot) {
+        /* select ECB Mode of Operation */
+        robot.clickOn("#DesModeOfOperationComboBox");
+        robot.clickOn("CBC");
+
+        /* select noPadding */
+        robot.clickOn("#DesPaddingComboBox");
+        robot.clickOn("NoPadding");
+
+        /* write input */
+        robot.clickOn("#DESInputTextArea").write("0000000000000000");
+
+        /* write ICV */
+        robot.clickOn("#DESICVTextField").write("0123456789ABCDEF");
+
+        /* write key */
+        robot.clickOn("#DESKeyTextField").write("0123456789ABCDEF");
+
+        /* click on Encrypt button */
+        robot.clickOn("#DESEncryptButton");
+
+        /* verify if encryption was successful */
+        FxAssert.verifyThat("#DesOutputTextArea", TextInputControlMatchers.hasText("56cc09e7cfdc4cef"));
+    }
 }
